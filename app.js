2b8661eb74a28bda1339b347441525f812f66e68
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-02-23 09:37:06 -0500

    Added functionality for bio and projects buttons to open content on click

--- app.js
+++ app.js
@@ -13,8 +13,25 @@
 ////////////////////////////////////////////
 $(()=>{
 
+    //Nav Button Functionality
 
+    //Bio
+    const $bio = $("#bio-button");
+    const $bioText = $("#bio-text");
 
+    const openBio = () => {
+      $bioText.css("display", "block");
+    };
 
+    $bio.on("click", openBio);
 
+    //Projects
+    const $projects = $("#projects-button")
+    const $projectsPhoto = $("#projects-photo")
+
+    const openProjects = () => {
+        $projectsPhoto.css("display", "block")
+    }
+
+    $projects.on("click", openProjects)
 })
