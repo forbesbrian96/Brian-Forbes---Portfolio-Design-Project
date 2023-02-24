15de5addf542923d1b26fccb0989be90686e5049
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-02-23 19:28:12 -0500

    Functionality for entire nav bar is complete. Flex arrangement for all pages is complete. Still need to write functionality for display theme changes, and give every theme a complete overhaul of color design

--- app.js
+++ app.js
@@ -15,23 +15,57 @@ $(()=>{
 
     //Nav Button Functionality
 
+    //Home 
+    const $home = $("#home-button")
+    const $homeText = $("#home-text")
+
+    const openHome = () => {
+        $homeText.css("display", "flex")
+        $bioText.css("display", "none")
+        $projectsPhoto.css("display", "none")
+    }
+
+    
+    $home.on("click", openHome)
+
     //Bio
     const $bio = $("#bio-button");
     const $bioText = $("#bio-text");
+    const $bioPhotos = $("bio-photo")
 
     const openBio = () => {
-      $bioText.css("display", "block");
+      $bioText.css("display", "flex")
+      $bioPhotos.css("display", "flex")
+      $homeText.css("display", "none")
+      $projectsPhoto.css("display", "none")
     };
 
     $bio.on("click", openBio);
 
     //Projects
     const $projects = $("#projects-button")
-    const $projectsPhoto = $("#projects-photo")
+    const $projectsPhoto = $("#projects-photo-row")
 
     const openProjects = () => {
-        $projectsPhoto.css("display", "block")
+        $projectsPhoto.css("display", "flex")
+        $homeText.css("display", "none")
+        $bioText.css("display", "none")
     }
 
     $projects.on("click", openProjects)
+
+    //Display Themes
+
+    // function myFunction() {
+    //     document.getElementById("myDropdown").classList.toggle("show");
+    //   }
+
+    const $displayThemes = $("#display-themes-button")
+    const $dropdown = $("#dropdown")
+
+   const dropDownToggle = () => {
+    $dropdown.toggle(500);
+   }
+
+    $displayThemes.on("click", dropDownToggle)
 })
